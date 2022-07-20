eeddf636772e67743c8abe8d5e99177b0e5d1d60
Ema322 <carlosema322@gmail.com>
2022-07-20 12:17:10 -0600

    Se configuran las primeras 2 opciones

--- Prueba1.py
+++ Prueba1.py
@@ -1,10 +1,10 @@
 n1 = float(input("Introduce tu primer número: ") )
 n2 = float(input("Introduce tu segundo número: ") )
+
 opcion = 0
 while True:
     print("""
-    Dime, ¿qué quieres hacer?2
-    
+    Dime, ¿qué quieres hacer?
     
     1) Sumar los dos números
     2) Restar los dos números
@@ -12,4 +12,14 @@ while True:
     4) Cambiar los números elegidos
     5) Apagar calculadora
     """)
-opcion = int(input("Elige una opción: ") )     
+    opcion = int(input("Elige una opción: ") )     
+
+    if opcion == 1:
+        print(" ")
+        print("RESULTADO: La suma de",n1,"+",n2,"es igual a",n1+n2)
+    elif opcion == 2:
+        print(" ")
+        print("RESULTADO: La resta de",n1,"-",n2,"es igual a",n1-n2)
+        break
+    else:
+        print("Opción incorrecta")
